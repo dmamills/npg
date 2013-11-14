372a2860a510473982492a0db38fbf82149be979
Daniel Mills <mills.dma@gmail.com>
2013-11-13 23:57:15 -0500

    param check

--- test/test.js
+++ test/test.js
@@ -1,14 +1,6 @@
 var should = require('should');
-	//npg = require('./../index');
+	npg = require('./../index');
 
 describe('npg',function() {
-	it('test',function() {
-		true.should.be.true;
-	});
-	it('should exist',function(){
-		var x = { lol:'lol'}
-		x.lol.should.equal('lol');
-		x.lol.should.be.ok;
-		x.should.be.ok;
-	});
+	
 });
